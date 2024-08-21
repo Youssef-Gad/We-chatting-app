c1d581bd15a1f42d2923806d5229f6a94ddeb711
Youssef_Gad <youssef.gad2003@yahoo.com>
2024-08-21 14:34:52 +0300

    Add Register Page

--- src/components/register/Register.jsx
+++ src/components/register/Register.jsx
@@ -1,5 +1,20 @@
+import RegisterForm from "./RegisterForm";
+
 function Register() {
-  return <div>Register</div>;
+  return (
+    <div className="h-[140vh] bg-light-gray flex items-center justify-center">
+      <div className="w-[600px] bg-white p-10 rounded-2xl shadow-md">
+        <div className="text-center text-dark-gray ">
+          <p className=" text-4xl font-semibold mb-2">Let's get started</p>
+          <p className="mb-5">
+            Join us here,a better place for every conversation
+          </p>
+        </div>
+
+        <RegisterForm />
+      </div>
+    </div>
+  );
 }
 
 export default Register;
