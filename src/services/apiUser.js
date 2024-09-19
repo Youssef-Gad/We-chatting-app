26823fe819236b8fbc0c32c9cbd8b79e2c55142e
Youssef_Gad <youssef.gad2003@yahoo.com>
2024-09-19 14:39:35 +0300

    Update apiUser To Send Form Data

--- src/services/apiUser.js
+++ src/services/apiUser.js
@@ -1,7 +1,5 @@
 import { BASE_URL } from "../constants/constants";
 
-
-
 export async function getUser() {
   try {
     const token = localStorage.getItem("token");
@@ -26,10 +24,9 @@ export async function updateUser(newData) {
     const res = await fetch(`${BASE_URL}/user`, {
       method: "PUT",
       headers: {
-        "Content-Type": "application/json",
         Authorization: `Bearer ${token}`,
       },
-      body: JSON.stringify(newData),
+      body: newData,
     });
 
     const data = await res.json();
